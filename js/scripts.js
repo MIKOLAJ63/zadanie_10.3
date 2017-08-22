ffc211eff69ab435c41ef38f63318587c85f479d
MIKOLAJ63 <MIKOLAJ63>
2017-08-22 17:49:21 +0200

    Carousel jQuery

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+//scripts.js
+$(function(){
+	console.log('DOM loaded - you can have fun');
+});
+
+$(function(){
+	var carouselList = $("#carousel ul");
+  setInterval(changeSlide, 4000); 
+  function changeSlide() {
+    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+  };
+  changeSlide();
+  function moveFirstSlide() {
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  };  
+});
+
+$('#move-left').on('click', function(){
+		var $last = $('#carousel li:last');
+		$last.remove().css({ 'margin-left': '0px' });
+		$('#carousel  li:first').before($last);
+		$last.animate({ 'margin-left': '0px' }, 500);
+    checkMark();
+	});
+
+	$('#move-right').on('click', function(){
+		var $first = $('#carousel li:first');
+		$first.animate({ 'margin-left': '-400px' }, 500, function() {
+			$first.remove().css({ 'margin-left': '0px' });
+			$('#carousel li:last').after($first);
+		});
+    checkMark();
+	});
